d9e04875c52544fc71f10bc95a77e8188ca27f06
sumit-sharma7 <sumit.softprodigy@gmail.com>
2017-10-31 19:19:00 +0530

    Satellizer authentication and logout

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -10,4 +10,10 @@ export class AuthController {
 			vm.$auth.setToken(token);
 		});
 	}
+	login() {
+		var vm = this;
+		this.$auth.login(this.login.user).then(function(token){
+			vm.$auth.setToken(token);
+		});
+	}
 }
